58d80a5c57d64efceb27508e270b0ee1661ecdcc
CurwaBober <dkarter513@gmail.com>
2024-12-10 12:48:37 +0300

    Удаление лишнего

--- main/app/build.gradle.kts
+++ main/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.rksihackaton"
-        minSdk = 33
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
